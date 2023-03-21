77e91d891e1e85d69cf4970c8d23a6267d9b6780
Maxime <maxilefranc@gmail.com>
2023-03-21 13:11:07 +0100

    sort movies by genre DONE

--- src/Controller/Frontoffice/GenreController.php
+++ src/Controller/Frontoffice/GenreController.php
@@ -3,26 +3,34 @@
 namespace App\Controller\Frontoffice;
 
 use App\Repository\GenreRepository;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class GenreController extends AbstractController
 {
-    #[Route('/frontoffice/genre/{name<\w+|\W+>}', name: 'app_frontoffice_genre_read')]
-    public function read(string $name, GenreRepository $genreRepository): Response
+    #[Route('/frontoffice/genre/{id<\d+>}', name: 'app_frontoffice_genre_read')]
+    public function read(int $id, GenreRepository $genreRepository, PaginatorInterface $paginatorInterface, Request $request): Response
     {
-        $genre = $genreRepository->findOneBy(['name' => $name]);
+        $genre = $genreRepository->find($id);
         if (!$genre) {
-            throw $this->createNotFoundException('Pas de genre trouvé avec ce nom: ' . $name);
+            throw $this->createNotFoundException('Pas de genre trouvé avec cet id: ' . $id);
         }
         $genreName = $genre->getName();
 
+        $movieToShow = $paginatorInterface->paginate(
+            $genre->getMovies(), // Requête contenant les données à paginer 
+            $request->query->getInt('page', 1), // Numéro de la page en cours, passé dans l'URL, 1 si aucune page
+            6 // Nombre de résultats par page
+        );
+
         return $this->render('frontoffice/genre/index.html.twig', [
             'allGenres' => $genreRepository->findAll(),
-            'genre' => $genre,
-            'genreName' => $genre->getName(),
-            'title' => $genre->getName() . ': films'
+            'movieToShow' => $movieToShow,
+            'genreName' => $genreName,
+            'title' => $genreName . ': films'
         ]);
     }
 }
--- src/Controller/Frontoffice/MainController.php
+++ src/Controller/Frontoffice/MainController.php
@@ -15,87 +15,87 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class MainController extends AbstractController
 {
-    #[Route('/', name: 'movie_browse')]
-    public function browse(MovieRepository $movieRepository, GenreRepository $genreRepository, PaginatorInterface $paginator, Request $request): Response
-    {
-        $allMovies = $movieRepository->findAll();
+  #[Route('/', name: 'movie_browse')]
+  public function browse(MovieRepository $movieRepository, GenreRepository $genreRepository, PaginatorInterface $paginator, Request $request): Response
+  {
+    $allMovies = $movieRepository->findAll();
 
-        $movieToShow = $paginator->paginate(
-            $allMovies, // Requête contenant les données à paginer (ici nos articles)
-            $request->query->getInt('page', 1), // Numéro de la page en cours, passé dans l'URL, 1 si aucune page
-            6 // Nombre de résultats par page
-        );
+    $movieToShow = $paginator->paginate(
+      $allMovies, // Requête contenant les données à paginer 
+      $request->query->getInt('page', 1), // Numéro de la page en cours, passé dans l'URL, 1 si aucune page
+      6 // Nombre de résultats par page
+    );
 
-        return $this->render('frontoffice/main/index.html.twig', [
-            'title' => "Bienvenue sur O'flix !",
-            'allMovies' => $movieToShow,
-            'allGenres' => $genreRepository->findAll(),
-        ]);
+    return $this->render('frontoffice/main/index.html.twig', [
+      'title' => "Bienvenue sur O'flix !",
+      'allMovies' => $movieToShow,
+      'allGenres' => $genreRepository->findAll(),
+    ]);
+  }
+
+  #[Route('/movie/{slug<[a-zA-Z0-9_-]+>}', name: 'movie_read')]
+  public function read($slug, MovieRepository $movieRepository, CastingRepository $castingRepository): Response
+  {
+    $movie = $movieRepository->findOneBy(['slug' => $slug]);
+
+    if (!$movie) {
+      throw $this->createNotFoundException('Pas de film ou serie trouvé avec le titre ' . $slug);
     }
 
-    #[Route('/movie/{slug<[a-zA-Z0-9_-]+>}', name: 'movie_read')]
-    public function read($slug, MovieRepository $movieRepository, CastingRepository $castingRepository): Response
-    {
-        $movie = $movieRepository->findOneBy(['slug' => $slug]);
+    $allCastingFromMovie = $castingRepository->findBy(['movie' => $movie->getId()], ['creditOrder' => 'ASC']);
 
-        if (!$movie) {
-            throw $this->createNotFoundException('Pas de film ou serie trouvé avec le titre ' . $slug);
-        }
+    return $this->render('frontoffice/main/read.html.twig', [
+      'title' => "O'flix - détail",
+      'movie' => $movie,
+      'castings' => $allCastingFromMovie,
+    ]);
+  }
 
-        $allCastingFromMovie = $castingRepository->findBy(['movie' => $movie->getId()], ['creditOrder' => 'ASC']);
+  #[Route('/movie/{id<\d+>}/edit', name: 'movie_edit')]
+  public function edit(int $id, MovieRepository $movieRepository, GenreRepository $genreRepository, EntityManagerInterface $entityManagerInterface): Response
+  {
+    $movieToUpdate = $movieRepository->find($id);
 
-        return $this->render('frontoffice/main/read.html.twig', [
-            'title' => "O'flix - détail",
-            'movie' => $movie,
-            'castings' => $allCastingFromMovie,
-        ]);
+    if (!$movieToUpdate) {
+      throw $this->createNotFoundException('Pas de film à l\'id ' . $id);
     }
 
-    #[Route('/movie/{id<\d+>}/edit', name: 'movie_edit')]
-    public function edit(int $id, MovieRepository $movieRepository, GenreRepository $genreRepository, EntityManagerInterface $entityManagerInterface): Response
-    {
-        $movieToUpdate = $movieRepository->find($id);
+    $allGenre = $genreRepository->findAll();
+    $randNumber = rand(0, count($allGenre) - 1);
+    $randGenre = $allGenre[$randNumber];
 
-        if (!$movieToUpdate) {
-            throw $this->createNotFoundException('Pas de film à l\'id ' . $id);
-        }
+    $movieToUpdate->addGenre($randGenre);
+    $movieToUpdate->setTitle('test');
 
-        $allGenre = $genreRepository->findAll();
-        $randNumber = rand(0, count($allGenre) - 1);
-        $randGenre = $allGenre[$randNumber];
+    $entityManagerInterface->flush();
 
-        $movieToUpdate->addGenre($randGenre);
-        $movieToUpdate->setTitle('test');
+    return $this->redirectToRoute('movie_read', ['id' => $id]);
+  }
 
-        $entityManagerInterface->flush();
+  #[Route('/movie/add', name: 'movie_add')]
+  public function add(MovieRepository $movieRepository): Response
+  {
+    $newMovie = new Movie();
+    $newMovie->setTitle('Mon nouveau film');
+    $newMovie->setDuration(500);
+    $newMovie->setType('film');
 
-        return $this->redirectToRoute('movie_read', ['id' => $id]);
+    $movieRepository->save($newMovie, true);
+
+    return $this->redirectToRoute('movie_browse');
+  }
+
+  #[Route('/movie/{id<\d+>}/delete', name: 'movie_delete')]
+  public function delete(int $id, MovieRepository $movieRepository): Response
+  {
+    $movieToDelete = $movieRepository->find($id);
+
+    if (!$movieToDelete) {
+      throw $this->createNotFoundException('Pas de film trouvé avec l\id ' . $id);
     }
 
-    #[Route('/movie/add', name: 'movie_add')]
-    public function add(MovieRepository $movieRepository): Response
-    {
-        $newMovie = new Movie();
-        $newMovie->setTitle('Mon nouveau film');
-        $newMovie->setDuration(500);
-        $newMovie->setType('film');
+    $movieRepository->remove($movieToDelete, true);
 
-        $movieRepository->save($newMovie, true);
-
-        return $this->redirectToRoute('movie_browse');
-    }
-
-    #[Route('/movie/{id<\d+>}/delete', name: 'movie_delete')]
-    public function delete(int $id, MovieRepository $movieRepository): Response
-    {
-        $movieToDelete = $movieRepository->find($id);
-
-        if (!$movieToDelete) {
-            throw $this->createNotFoundException('Pas de film trouvé avec l\id ' . $id);
-        }
-
-        $movieRepository->remove($movieToDelete, true);
-
-        return $this->redirectToRoute('movie_browse');
-    }
+    return $this->redirectToRoute('movie_browse');
+  }
 }
